fd3cdd4bb6c1965764b3973442f5f049ee2d540d
David Bauer <mail@david-bauer.net>
2018-03-15 23:43:50 +0100

    handle exception in case mls query returns no result

--- domain_director/blueprints.py
+++ domain_director/blueprints.py
@@ -2,7 +2,7 @@ import json
 from ipaddress import AddressValueError
 
 from flask import Blueprint, request, current_app, jsonify
-from mozls import WifiNetwork, query_mls
+from mozls import WifiNetwork, query_mls, MLSException
 
 from domain_director import ipv6_to_mac
 from domain_director.domain import decide_node_domain
@@ -15,17 +15,19 @@ def serve():
     revisit = False
     domain = None
     wifis = request.form["wifis"]
-
-    mls_response = query_mls(
-        wifi_networks=[WifiNetwork(mac_address=ap["bssid"], signalStrength=int(ap["signal"]))
-                       for ap in json.loads(wifis)],
-        apikey=current_app.config["MLS_API_KEY"])
+    try:
+        mls_response = query_mls(
+            wifi_networks=[WifiNetwork(mac_address=ap["bssid"], signalStrength=int(ap["signal"]))
+                           for ap in json.loads(wifis)],
+            apikey=current_app.config["MLS_API_KEY"])
+    except MLSException:
+        mls_response = None
     try:
         node_id = ipv6_to_mac(request.remote_addr).replace(':', '')
         domain = decide_node_domain(node_id=node_id,
-                                    lat=mls_response.lat,
-                                    lon=mls_response.lon,
-                                    accuracy=mls_response.accuracy,
+                                    lat=mls_response.lat if mls_response else None,
+                                    lon=mls_response.lon if mls_response else None,
+                                    accuracy=mls_response.accuracy if mls_response else None,
                                     polygons=current_app.domain_polygons,
                                     default_domain=current_app.config["DEFAULT_DOMAIN"])
         if domain == current_app.config["DEFAULT_DOMAIN"]:
