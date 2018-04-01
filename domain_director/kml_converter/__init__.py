85e73abc9ce0db07940f8fd3b6174d1ea6d6bf04
David Bauer <mail@david-bauer.net>
2018-04-01 21:42:01 +0200

    fetch domaincode from extended data

--- domain_director/kml_converter/__init__.py
+++ domain_director/kml_converter/__init__.py
@@ -3,13 +3,25 @@ from fastkml import kml
 from geojson import FeatureCollection, Feature, Polygon
 
 
+def get_domaincode(data, fieldnames=None):
+    if fieldnames is None:
+        fieldnames = ["Description", "description"]
+
+    if data.extended_data is not None:
+        for df in data.extended_data.elements:
+            for name in fieldnames:
+                if df.name == name and len(df.value) > 0:
+                    return df.value
+    return data.description
+
+
 def convert_kml_to_geojson(kml_input):
     k = kml.KML()
     k.from_string(kml_input)
 
     features = FeatureCollection([Feature(
         geometry=Polygon([[(coordinate[0], coordinate[1]) for coordinate in area.geometry.exterior.coords]]),
-        properties={"name": area.description, "pretty_name": area.name}
+        properties={"name": get_domaincode(area), "pretty_name": area.name}
     ) for area in list(k.features()).pop().features()])
 
     return geojson.dumps(features)
