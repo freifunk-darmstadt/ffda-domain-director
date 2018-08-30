e036d94571e3ab20a3b7d7105fac1185259032cf
David Bauer <mail@david-bauer.net>
2018-08-30 19:50:41 +0200

    apply kml color to geojson

--- domain_director/kml_converter/__init__.py
+++ domain_director/kml_converter/__init__.py
@@ -1,5 +1,5 @@
 import geojson
-from fastkml import kml
+from fastkml import kml, LineStyle, PolyStyle
 from geojson import FeatureCollection, Feature, Polygon
 
 
@@ -19,9 +19,17 @@ def convert_kml_to_geojson(kml_input):
     k = kml.KML()
     k.from_string(kml_input)
 
+    a = list(list(k.features()).pop().features())[0].styleUrl
+
+    styles = list(list(k.features()).pop().styles())
     features = FeatureCollection([Feature(
         geometry=Polygon([[(coordinate[0], coordinate[1]) for coordinate in area.geometry.exterior.coords]]),
-        properties={"name": get_domaincode(area), "pretty_name": area.name}
+        properties={"name": get_domaincode(area),
+                    "pretty_name": area.name,
+                    "color": "#" + [polystyle for polystyle in [list(style.styles()) for style in styles
+                                                               if area.styleUrl[1:] + '-normal' == style.id][0]
+                                   if type(polystyle) == PolyStyle][0].color[2:]
+                    }
     ) for area in list(k.features()).pop().features()])
 
     return geojson.dumps(features)
