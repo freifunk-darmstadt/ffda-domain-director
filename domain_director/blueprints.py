9fb1cc41cf2e6a5734f500a752970938ec049151
David Bauer <mail@david-bauer.net>
2018-09-15 02:48:25 +0200

    simplify response object
    remove unused location fields from response
    
    remove revisit feature as it was not implemented into the client

--- domain_director/blueprints.py
+++ domain_director/blueprints.py
@@ -1,6 +1,5 @@
 import json
 from flask import Blueprint, request, current_app, jsonify, render_template
-from ipaddress import AddressValueError
 from mozls import WifiNetwork, query_mls, MLSException
 
 from domain_director import ipv6_to_mac
@@ -12,9 +11,6 @@ bp = Blueprint('domain_director', __name__)
 
 @bp.route('/', methods=['GET', 'POST'])
 def serve():
-    revisit = False
-    domain = None
-
     wifis = request.form["wifis"]
     try:
         mls_response = query_mls(
@@ -23,30 +19,22 @@ def serve():
             apikey=current_app.config["MLS_API_KEY"])
     except MLSException:
         mls_response = None
-    try:
-        ip_address = request.headers.get("X-Real-IP", None) or request.remote_addr
-        node_id = ipv6_to_mac(ip_address).replace(':', '')
-        domain, switch_time = get_node_domain(node_id=node_id,
-                                 lat=mls_response.lat if mls_response else None,
-                                 lon=mls_response.lon if mls_response else None,
-                                 accuracy=mls_response.accuracy if mls_response else None,
-                                 polygons=current_app.domain_polygons,
-                                 default_domain=current_app.config["DEFAULT_DOMAIN"],
-                                 switch_time=current_app.config["DOMAIN_SWITCH_TIME"],
-                                 migrate_only_vpn=current_app.config["ONLY_MIGRATE_VPN"])
-        if domain == current_app.config["DEFAULT_DOMAIN"]:
-            revisit = True
-    except AddressValueError:
-        revisit = True
+    ip_address = request.headers.get("X-Real-IP", None) or request.remote_addr
+    node_id = ipv6_to_mac(ip_address).replace(':', '')
+    domain, switch_time = get_node_domain(node_id=node_id,
+                             lat=mls_response.lat if mls_response else None,
+                             lon=mls_response.lon if mls_response else None,
+                             accuracy=mls_response.accuracy if mls_response else None,
+                             polygons=current_app.domain_polygons,
+                             default_domain=current_app.config["DEFAULT_DOMAIN"],
+                             switch_time=current_app.config["DOMAIN_SWITCH_TIME"],
+                             migrate_only_vpn=current_app.config["ONLY_MIGRATE_VPN"])
     return jsonify({
         "node_information": {
-            "location": {"lat": mls_response.lat,
-                         "lon": mls_response.lon,
-                         "accuracy": mls_response.accuracy, },
             "domain": {
                 "name": domain,
                 "switch_time": switch_time,
-                "revisit": revisit, }
+            }
         }})
 
 
